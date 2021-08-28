d8ac536938fba3368abaf9546d172727b8674a13
William Serna <wsernalaverde@gmail.com>
2021-08-27 20:45:02 -0500

    Add entity

---
+++ willtodo.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace willtodo.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
